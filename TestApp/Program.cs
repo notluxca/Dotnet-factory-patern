62775b1cdaf369119998909f44ab0a9094969d70
notluxca <lucasrochafernandes20@gmail.com>
2025-09-21 02:23:21 -0300

    :update: Implemented type reflection

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AbilityFactory
+﻿using System.Reflection;
+
+namespace AbilityFactory
 {
     public enum AbilityType
     {
@@ -9,43 +11,51 @@
 
     public interface IAbility
     {
-        public string Name { get; }
+        public AbilityType Type { get; }
         public void Use();
     }
 
     public class FireStrike : IAbility
     {
-        public string Name => "fire_strike";
+        public AbilityType Type => AbilityType.FireStike;
 
-        public void Use()
-        {
-            Console.WriteLine("Casting Fire Strike!");
-        }
+        public void Use() => Console.WriteLine("Casting Fire Strike!");
     }
 
     public class Heal : IAbility
     {
-        public string Name => "heal";
-
-        public void Use()
-        {
-            Console.WriteLine("Casting Heal!");
-        }
+        public AbilityType Type => AbilityType.Heal;
+        public void Use() => Console.WriteLine("Casting Heal!");
     }
 
     public class Teleport : IAbility
     {
-        public string Name => "teleport";
-
-        public void Use()
-        {
-            Console.WriteLine("Teleporting!");
-        }
+        public AbilityType Type => AbilityType.Teleport;
+        public void Use() => Console.WriteLine("Teleporting!");
     }
 
     // Ability Factory responsible for creating new abilities.
-    public static class AbilityFactory //! still breaks open closed, cause to extend new abilities this class must be modified
+    public static class AbilityFactory
     {
+
+        private static Dictionary<AbilityType, Type> abilitiesByType;
+        private static bool IsInitialized => abilitiesByType != null;
+
+        private static void InitializeFactory() //* Calling this because there is no constructor on a static class because it is not instantiated
+        {
+            if (IsInitialized) return;
+
+            var abilitiesTypes = Assembly.GetAssembly(typeof(IAbility)).GetTypes().Where(myType => myType.IsClass && !myType.IsAbstract && typeof(IAbility).IsAssignableFrom(myType));
+
+            abilitiesByType = new Dictionary<AbilityType, Type>();
+
+            foreach (var type in abilitiesTypes)
+            {
+                var tempEffect = Activator.CreateInstance(type) as IAbility;
+                abilitiesByType.Add(tempEffect.Type, type);
+            }
+        }
+
         public static IAbility CreateAbility(AbilityType type)
         {
             switch (type)
@@ -60,15 +70,27 @@
                     throw new ArgumentException("Tipo de habilidade inválido!");
             }
         }
-    }
 
+        public static IAbility GetAbility(AbilityType abilityType)
+        {
+            InitializeFactory();
+            if (abilitiesByType.ContainsKey(abilityType))
+            {
+                Type type = abilitiesByType[abilityType];
+                var ability = Activator.CreateInstance(type) as IAbility;
+                return ability;
+            }
+
+            return null;
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            IAbility fireSpell = AbilityFactory.CreateAbility(AbilityType.FireStike);
-            fireSpell.Use();
+            IAbility ability = AbilityFactory.GetAbility(AbilityType.FireStike);
+            ability.Use();
         }
     }
 }
